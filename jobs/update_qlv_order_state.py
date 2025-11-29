b58fb1be9a73bb75c4f144384e43384b67470d23
ckf10000 <ckf10000@sina.com>
2025-11-29 15:17:52 +0800

    1. 新增劲旅平台登录状态更新的任务

--- jobs/update_qlv_order_state.py
+++ jobs/update_qlv_order_state.py
@@ -42,43 +42,44 @@ async def update_order_state(
     key = await redis_client.rpop(key=list_key)
     if not key:
         return "Redis队列中没有需要更新状态的订单数据，任务跳过"
-    playwright_state = await redis_client.get(key=redis_client.gen_qlv_login_state_key())
-    if not playwright_state:
+    try:
+        playwright_state = await redis_client.get(key=redis_client.gen_qlv_login_state_key())
+        if not playwright_state:
+            raise RuntimeError("Redis中劲旅登录状态数据已过期")
+        timeout: int = 5
+        retry: int = 1
+        order_id = key.split(":")[-1]
+        order_info = await redis_client.get(key=key)
+        if not order_info:
+            return f"劲旅订单：{order_id}，在Redis中的详情数据已经过期，任务跳过"
+        response: Dict[str, Any] = await get_order_info_with_http(
+            order_id=order_id, timeout=timeout, domain=domain, protocol=protocol, enable_log=True, retry=retry,
+            cookie_jar=CookieJar(), playwright_state=playwright_state
+        )
+        """"
+        消息结构：
+        {'code': 200, 'message': '订单出票查看', 'data': {'OTA实收': '1952.70', '佣金': '0.00', '订单号': '160669', '平台订单号': '[4646735080-4646735080]', '行程类型': '单程', '订单状态': '待处理', '订单操作': '收款完成'}}
+        """
+        if response.get("code") != 200 or response.get("message") != "订单出票查看":
+            raise RuntimeError(str(response))
+        data = response.get("data")
+        stat_order = data.get("订单状态")
+        stat_opration = data.get("订单操作")
+        if not stat_order or not stat_opration:
+            raise RuntimeError(f"劲旅订单：{order_id}，详情页面数据解析异常")
+        if discard_state:
+            if stat_order in discard_state:
+                await redis_client.expire(key=key, expire=1)
+                return f"劲旅订单：{order_id}，当前的订单状态：{stat_order}，在Redis中的详情数据将被丢弃"
+        order_info["stat_order"] = stat_order
+        order_info["stat_opration"] = stat_opration
+        ttl = await redis_client.ttl(key=key)
+        await redis_client.set(key=key, value=order_info, ex=ttl)
         await redis_client.lpush(key=list_key, value=key)
-        raise RuntimeError("Redis中劲旅登录状态数据已过期")
-    timeout: int = 5
-    retry: int = 1
-    order_id = key.split(":")[-1]
-    order_info = await redis_client.get(key=key)
-    if not order_info:
-        return f"劲旅订单：{order_id}，在Redis中的详情数据已经过期，任务跳过"
-    response: Dict[str, Any] = await get_order_info_with_http(
-        order_id=order_id, timeout=timeout, domain=domain, protocol=protocol, enable_log=True, retry=retry,
-        cookie_jar=CookieJar(), playwright_state=playwright_state
-    )
-    """"
-    消息结构：
-    {'code': 200, 'message': '订单出票查看', 'data': {'OTA实收': '1952.70', '佣金': '0.00', '订单号': '160669', '平台订单号': '[4646735080-4646735080]', '行程类型': '单程', '订单状态': '待处理', '订单操作': '收款完成'}}
-    """
-    if response.get("code") != 200 or response.get("message") != "订单出票查看":
+        return f"任务执行成功，劲旅订单：{order_id}，在Redis中的订单状态已更新"
+    except (Exception, RuntimeError) as e:
         await redis_client.lpush(key=list_key, value=key)
-        raise RuntimeError(str(response))
-    data = response.get("data")
-    stat_order = data.get("订单状态")
-    stat_opration = data.get("订单操作")
-    if not stat_order or not stat_opration:
-        await redis_client.lpush(key=list_key, value=key)
-        raise RuntimeError(f"劲旅订单：{order_id}，详情页面数据解析异常")
-    if discard_state:
-        if stat_order in discard_state:
-            await redis_client.expire(key=key, expire=1)
-            return f"劲旅订单：{order_id}，当前的订单状态：{stat_order}，在Redis中的详情数据将被丢弃"
-    order_info["stat_order"] = stat_order
-    order_info["stat_opration"] = stat_opration
-    ttl = await redis_client.ttl(key=key)
-    await redis_client.set(key=key, value=order_info, ex=ttl)
-    await redis_client.lpush(key=list_key, value=key)
-    return f"任务执行成功，劲旅订单：{order_id}，在Redis中的订单状态已更新"
+        raise RuntimeError(str(e))
 
 
 def register(executor):
